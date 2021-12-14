ee2052cfc82db86391d4f630cb63099d95d3ccf8
Jachimo <jachimo@noreply.github.com>
2021-12-14 11:40:57 -0500

    Refactor to use argparse

--- mimewrapper.py
+++ mimewrapper.py
@@ -4,25 +4,37 @@
 import sys
 import os
 import email.mime
+import argparse
 
 def main() -> int:
-    input = sys.argv[1]
-    output = os.path.splitext(sys.argv[1])[0] + '.eml'
-    if os.path.isfile(os.path.splitext(sys.argv[1])[0] + '.headers'):
-        sidecar = os.path.splitext(sys.argv[1])[0] + '.headers'
-        headerlist = parse_sidecar_file(sidecar)
-    elif os.path.isfile(sys.argv[1] + '.headers'):
-        sidecar = sys.argv[1] + '.headers'
-        headerlist = parse_sidecar_file(sidecar)
-    else:
-        print('No sidecar file found, interactive mode not implemented.')
-        return 1
+    parser = argparse.ArgumentParser(description='Wrap a file in a MIME document using specified metadata.')
+    parser.add_argument('input', type=str, help='Specify input file to wrap into MIME')
+    parser.add_argument('-s', '--sidecar', type=str, help='Specify metadata sidecar file')
+    parser.add_argument('-o', '--output', type=str, nargs='?', help='Specify output file')
+    parser.add_argument('--debug', help='Enable debug mode (very verbose)', action='store_true')
+    args = parser.parse_args()
+
+    if not args.output:
+        args.output = os.path.splitext(args.input) + '.eml'  # default output filename
+
+    if not args.sidecar:
+        if os.path.isfile(os.path.splitext(args.input)[0] + '.headers'):
+            args.sidecar = os.path.splitext(args.input)[0] + '.headers'
+        elif os.path.isfile(args.input + '.headers'):
+            args.sidecar = args.input + '.headers'
+        else:
+            print('No sidecar file found, interactive mode not implemented.')
+            return 1
+
+    headerlist = parse_sidecar_file(args.sidecar)
+
     msg = email.message.EmailMessage
     for h in headerlist:
         msg.add_header(h[0], h[1])  # set headers on output message
-    msg.add_attachment(input)  # TODO may need to open and pass file object?
-    with open(output, 'wb') as of:
-        of.write(msg.as_bytes())
+    with open(args.input, 'rb') as inputf:
+        msg.add_attachment(inputf)  # TODO may need to open and pass file object?
+    with open(args.output, 'wb') as outputf:
+        outputf.write(msg.as_bytes())
     return 0
 
 
@@ -36,7 +48,7 @@ def parse_sidecar_file(sidecar: str) -> list:
         if (': ' in l) or (':\t' in l):
             header = l.split(':', 1)[0].strip()
             value = l.split(':', 1)[1].strip()
-            headerlist.append( tuple((header, value)) )
+            headerlist.append(tuple((header, value)))  # TODO are double () really needed?
     return headerlist
 
 
